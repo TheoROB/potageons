c458befc33c223f7d7a5be9ebd64e241e6a04dae
TheoROB <theorobillard@live.com>
2022-10-06 15:37:53 +0200

    done abonement pages

--- pages/abonnement.js
+++ pages/abonnement.js
@@ -3,27 +3,28 @@ import Layout from '../components/Layout'
 import eric from '../assets/eric.jpg'
 import sarah from '../assets/sarah.PNG'
 import ema from '../assets/ema.PNG'
+import Image from 'next/image'
+
 
 function Abonnement() {
   return (
     <Layout>
       <div>
-        <p class="text gras">Abonnement(s)</p>
-        <table>
-            <tr>
-                <td><img src={eric} class="aboImg" alt="PP Eric" /></td>  
-                <td>Eric</td>             
-            </tr>
-            <tr>
-                <td><img src={sarah} class="aboImg" alt="PP Sarah" /></td> 
-                <td>Sarah</td>       
-            </tr>
-            <tr>
-                <td><img src={ema} class="aboImg" alt="PP Ema" /></td>
-                <td>Ema</td> 
-            </tr>
-
-        </table>       
+        <p class="text-xl m-5">Abonnement(s)</p>
+        <div className='m-5 flex flex-col h-44 justify-between'>
+          <div className='flex items-center'>
+            <Image src={eric} class="profilImg" alt="PP Eric" width={50} height={50} />
+            <p className='ml-5'>Eric</p>
+          </div>
+          <div className='flex items-center'>
+            <Image src={sarah} class="profilImg" alt="PP Sarah" width={50} height={50} />
+            <p className='ml-5'>Sarah</p>
+          </div>
+          <div className='flex items-center'>
+            <Image src={ema} class="profilImg" alt="PP Ema" width={50} height={50} />
+            <p className='ml-5'>Ema</p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
