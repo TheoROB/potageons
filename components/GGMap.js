2f6c754d7a0eecdff565ea31207fa0a4c54f4479
TheoROB <theorobillard@live.com>
2022-10-06 12:41:51 +0200

    done map

--- components/GGMap.js
+++ components/GGMap.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import map from '../assets/map.png'
+import Image from 'next/image'
 
 const GGMap = () => {
   return (
     <div>
-      <h1 className="font-bold text-xl mt-5">MAP:</h1>
-      <img className="mt-10" src={map} alt="this is car image" />
+      <h1 className="font-bold text-xl ml-3 mt-5 mb-10">MAP:</h1>
+      <div className="flex justify-center">
+        <Image className="mt-10" src={map} alt="this is car image" width={500} height={300} />
+      </div>
     </div>
   )
 }
