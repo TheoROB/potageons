d5a8c90c16d7669f7c791854d5697ec1d9c2a48c
TheoROB <theorobillard@live.com>
2022-10-06 15:30:12 +0200

    done profilUser page

--- pages/profilUser.js
+++ pages/profilUser.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Image from 'next/image'
+import usersolid from '/assets/circle-user-solid.svg'
 
 const ProfilUser = () => {
   return (
     <Layout>
-      <div>      
-        <i class="fa-solid fa-circle-user"></i>
-        <table class="profil">
-          <th>AbonnÃ©e(s)</th>
-          <th>Abonnement(s)</th>
-          <tr>
-            <td>0</td>
-            <td>3</td>
-          </tr>
-        </table>
-        <p class="text">Mail : nicolas.test@epsi.fr</p>
-    </div>
+      <div className="mt-10">      
+        <div className="flex flex-col items-center">
+        <Image src={usersolid} alt="" width={200} height={150} />
+          <p class="text-xl mt-5">Nicolas</p>
+        </div>
+        <div className="flex justify-evenly mt-10">
+          <div className="flex flex-col items-center">
+            <p className="text-lg">Abonné(s)</p>
+            <p>0</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-lg">Abonnement(s)</p>
+            <p>3</p>
+          </div>
+        </div>
+        <p className='mt-10 ml-5 text-xl'>Email: nico.test@epsi.fr</p>
+      </div>
     </Layout>
   )
 }
